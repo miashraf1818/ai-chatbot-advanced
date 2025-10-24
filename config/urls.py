e29a937c56dcb45b56f1bfde3e7cfa5761909af4
miashraf1818 <miashrafmiashraf18@gmail.com>
2025-10-24 18:28:32 +0530

    Add root health check endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,4 @@
 """
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-"""
 Main URL Configuration
 """
 
@@ -23,9 +6,34 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+
+
+@require_http_methods(["GET"])
+def root_health_check(request):
+    """Root health check endpoint"""
+    return JsonResponse({
+        'status': 'healthy',
+        'service': 'AI Chatbot API',
+        'version': '1.0.0',
+        'endpoints': {
+            'admin': '/admin/',
+            'chatbot_api': '/api/chatbot/',
+            'users_api': '/api/users/',
+            'analytics_api': '/api/analytics/',
+        }
+    })
+
 
 urlpatterns = [
+    # Root endpoint
+    path('', root_health_check, name='root'),
+
+    # Django admin
     path('admin/', admin.site.urls),
+
+    # API routes
     path('api/chatbot/', include('apps.chatbot.urls')),
     path('api/users/', include('apps.users.urls')),
     path('api/analytics/', include('apps.analytics.urls')),
